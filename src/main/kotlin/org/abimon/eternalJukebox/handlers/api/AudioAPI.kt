f6c61a1e0900b06f6f7bb1a7813dafd9c2dfd5b8
daviirodrig <git@davi.gq>
2023-06-21 18:11:02 -0300

    fix audio api end file

--- src/main/kotlin/org/abimon/eternalJukebox/handlers/api/AudioAPI.kt
+++ src/main/kotlin/org/abimon/eternalJukebox/handlers/api/AudioAPI.kt
@@ -252,7 +252,7 @@ object AudioAPI : IAPI {
                             val tmpFile = File("$uuid.tmp")
                             val tmpLog = File("$b64-$uuid.log")
                             val ffmpegLog = File("$b64-$uuid.log")
-                            val endGoalTmp = File(tmpFile.absolutePath.replace(".tmp", ".$format"))
+                            val endGoalTmp = File(tmpFile.absolutePath.replace(".tmp", ".tmp.$format"))
 
                             try {
                                 withContext(Dispatchers.IO) {
