048d3f5b12c410a34bec26ccdd13ec7ca76d4588
floriegl <22279483+floriegl@users.noreply.github.com>
2024-03-30 02:51:10 +0100

    Fixed downloading videos where the videoId starts with a minus

--- yt.sh
+++ yt.sh
@@ -1,1 +1,1 @@
-yt-dlp "$1" --audio-format $3 -x -o $2 --max-filesize 100m --no-playlist --max-downloads 1 --playlist-end 1 --exec "echo Video ID: %(id)s"
+yt-dlp --audio-format $3 -x -o $2 --max-filesize 100m --no-playlist --max-downloads 1 --playlist-end 1 --exec "echo Video ID: %(id)s" -- "$1"
